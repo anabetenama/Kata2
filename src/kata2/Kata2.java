d8115ebcd5aa71b39eefb843e9e0a49de9f15fb0
Entrar <Entrar@APD-L13>
2017-09-29 12:08:28 +0100

    Kata2.2: Frecuencia de cada valor en array

--- src/kata2/Kata2.java
+++ src/kata2/Kata2.java
@@ -9,15 +9,10 @@ public class Kata2 {
         int [] data = {1,1,1,100,2,4,5,6,8,8,4,5,6,2,-4};
         Map<Integer, Integer> histogram = new HashMap<>();
         
-        for (int i = 0; i < data.length; i++) {
-            if(histogram.containsKey(data[i])){
-                histogram.put(data[i], histogram.get(data[i]) + 1);
-            }else{
-                histogram.put(data[i], 1);
-            }
-            
+        for (int key : data) {
+            histogram.put(key, histogram.containsKey(key) ? 
+                    histogram.get(key) + 1 : 1);
         }
-        
         for (int key: histogram.keySet()){
             System.out.println(key + "--> " + histogram.get(key) + "veces");
             
